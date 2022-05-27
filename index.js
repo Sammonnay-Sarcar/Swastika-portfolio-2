92124d194ed33dcae3439e86889d3afa7edf0395
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2022-05-27 10:25:12 +0530

    colour-commit

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ let navbar = document.getElementById("navbar");
      navbar.classList.toggle("hidden");
 
  })
+
+var colorArray=["#FCD8D4", "#FDF6F0", "#F8E2CF", "#F5C6AA", "#FDD2BF", "#FCDEC0", "#FCDEC0"];
+var achievementCard= document.getElementsByClassName("achievement-card");
+
+for(var i =0;i<achievementCard.length;i++){
+    var cIndex= [Math.floor(Math.random()*7)];
+    achievementCard[i].style.backgroundColor= colorArray[cIndex];
+}
+
+var parameterCard= document.getElementsByClassName("parameter-card");
+
+for(var i =0;i<parameterCard.length;i++){
+    var cIndex= [Math.floor(Math.random()*7)];
+    parameterCard[i].style.backgroundColor= colorArray[cIndex];
+}
